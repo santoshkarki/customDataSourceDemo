c17c5a20089a1d8abbcf886dc4af34d93958c74f
Santosh Karki <santosh.karki044@gmail.com>
2020-04-12 10:30:48 -0500

    basic dataSource initial commit

---
+++ src/main/java/com/example/basicDataSourceDemo/BasicDataSourceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicDataSourceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDataSourceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicDataSourceDemoApplication.class, args);
+	}
+
+}
